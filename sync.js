ac2a35a17ef21888d12a1c8c1b8968105cb1898a
PassyahRaffi <passyah11@gmail.com>
2025-11-28 20:00:40 +0700

    update

--- sync.js
+++ sync.js
@@ -5,7 +5,7 @@ require("./models"); // penting! ini yang load semua relasi
 (async () => {
   try {
     console.log("⏳ Syncing database...");
-    await sequelize.sync({ alter: true });
+    await sequelize.sync({ force: true });
     console.log("✅ All tables synced with latest relations!");
     process.exit();
   } catch (err) {
